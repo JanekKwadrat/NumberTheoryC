823c9a914092059bb963f5ce5f669bc6dd6b0165
Jan Zakrzewski <janek.kwadrat@gmail.com>
2022-03-03 05:49:03 +0100

    added rng, implemented two variations of fibonacci numbers computing algorithms

--- ntlib.h
+++ ntlib.h
@@ -3,9 +3,19 @@
  * reversing mod sth
  * checking whether prime
  * computing fibonacci numbers
+ * computing fibonacci numbers faster then in log-time by preprocessing
  * getting number of coprimes below some value
  * 2D matrices computations
  * eratosthenes sieve and factorization
+ * powers of radicals
+ * computing binomials
+ * crt computation
+ * 
+ * symbolic computations
+ * 
+ * pseudorandom number generator
+ * 
+ * document code
  */
 
 #include "rustlike.h"
@@ -16,6 +26,8 @@ u64 reverse(u64, u64);
 
 u64 pow(u64, u64, u64);
 u64 fib(u64, u64);
+u64 fib1(u64, u64);
+u64 fibQ(u64);
 
 u64 sqrt(u64);
 
@@ -26,3 +38,7 @@ void mod_matrix2d(u64[2][2], u64);
 
 u8 fact(u64, u64*, u8*);
 void primality(u32, u8*);
+
+u64 seeder(u64);
+u32 rand32();
+u64 rand64();
--- test.cpp
+++ test.cpp
@@ -1,29 +1,102 @@
 #include <iostream>
+#include <ctime>
 #include "ntlib.h"
 
-/*void display_2x2(u64 arr[2][2]) {
-    std::cout << "{\n";
-    std::cout << "    " << arr[0][0] << "  " << arr[0][1] << "\n";
-    std::cout << "    " << arr[1][0] << "  " << arr[1][1] << "\n";
-    std::cout << "}\n";
-}*/
+void test();
+void test_fib();
+void gen_fib();
 
 int main() {
 
-    /*u64 a[2][2] = {{1, 1}, {1, 0}};
-    u64 b[2][2] = {{1, 1}, {1, 0}};
-    display_2x2(a);
-    mul_matrix2d(a, b);
-    display_2x2(a);*/
-
-    std::cout << fib(0, u64_max) << "\n";
-    std::cout << fib(1, u64_max) << "\n";
-    std::cout << fib(2, u64_max) << "\n";
-    std::cout << fib(3, u64_max) << "\n";
-    std::cout << fib(4, u64_max) << "\n";
-    std::cout << fib(5, u64_max) << "\n";
-    std::cout << fib(6, u64_max) << "\n";
-    std::cout << fib(7, u64_max) << "\n";
+    test_fib();
+    test();
     
     return 0;
+}
+
+void gen_fib() {
+    const u64 Q = 1e9 + 7;
+    std::cout << "{\n";
+    for(u64 i = 0; i < 8; ++i) {
+        std::cout << "    {";
+        for(u64 x = 0; x < 256; ++x) {
+            u64 itr = x << (8 * i);
+            std::cout << "{" << fib(itr - 1, Q) << ", " << fib(itr, Q) << ", " << fib(itr + 1, Q) << "}, ";
+        }
+        std::cout << "},\n";
+    }
+    std::cout << "}\n";
+}
+
+void test_fib() {
+    const u64 mod = 1e9 + 7;
+    i32 n = 10'000'000;
+    u64 f0 = 0, f1 = 1, f2;
+    bool fib_ok = true;
+    bool fib1_ok = true;
+    bool fibQ_ok = true;
+    for(i32 i = 0; i < n; ++i) {
+        if(f0 != fib(i, mod)) fib_ok = false;
+        if(f0 != fib1(i, mod)) fib1_ok = false;
+        if(f0 != fibQ(i)) fibQ_ok = false;
+        
+        f2 = f0 + f1;
+        f0 = f1 % mod;
+        f1 = f2 % mod;
+    }
+    std::cout << "fib " << (fib_ok ? "is correct" : "was wrong") << " on the first " << n << " numbers\n";
+    std::cout << "fib1 " << (fib1_ok ? "is correct" : "was wrong") << " on the first " << n << " numbers\n";
+    std::cout << "fibQ " << (fibQ_ok ? "is correct" : "was wrong") << " on the first " << n << " numbers\n";
+}
+
+void test() {
+    i32 seed = 2137;
+    i32 cases = 100'000'000;
+    clock_t beg, end;
+
+    seeder(seed);
+    beg = clock();
+    for(i32 q = cases; q--;) {
+        u64 k = rand64() % (u64)1e12;
+        u64 fib_ans = fibQ(k);
+    }
+    end = clock();
+
+    std::cout << "fibQ finsished " << cases << " cases in " << (double)(end - beg) / CLOCKS_PER_SEC << " s\n";
+    std::cout << "fibQ computes one case in " << (double)(end - beg) / CLOCKS_PER_SEC / (double)cases << " s on average\n";
+    
+    seeder(seed);
+    beg = clock();
+    for(i32 q = cases; q--;) {
+        u64 k = rand64() % (u64)1e12;
+        u64 M = rand64() % (u64)3e9;
+        u64 fib_ans = fib(k, M);
+    }
+    end = clock();
+
+    std::cout << "fib finsished " << cases << " cases in " << (double)(end - beg) / CLOCKS_PER_SEC << " s\n";
+    std::cout << "fib computes one case in " << (double)(end - beg) / CLOCKS_PER_SEC / (double)cases << " s on average\n";
+
+    seeder(seed);
+    beg = clock();
+    for(i32 q = cases; q--;) {
+        u64 k = rand64() % (u64)1e12;
+        u64 M = rand64() % (u64)3e9;
+        u64 fib_ans = fib1(k, M);
+    }
+    end = clock();
+
+    std::cout << "fib1 finsished " << cases << " cases in " << (double)(end - beg) / CLOCKS_PER_SEC << " s\n";
+    std::cout << "fib1 computes one case in " << (double)(end - beg) / CLOCKS_PER_SEC / (double)cases << " s on average\n";
+    
+    seeder(seed);
+    beg = clock();
+    for(i32 q = cases; q--;) {
+        u64 k = rand64() % (u64)1e12;
+        u64 M = rand64() % (u64)3e9;
+    }
+    end = clock();
+
+    std::cout << "rng finsished " << cases << " cases in " << (double)(end - beg) / CLOCKS_PER_SEC << " s\n";
+    std::cout << "rng randomizes one case in " << (double)(end - beg) / CLOCKS_PER_SEC / (double)cases << " s on average\n";
 }
